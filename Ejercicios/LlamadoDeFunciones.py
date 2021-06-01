d6fbdd01a68a96bf0ea77dd1c96bcc3d6f8e95cd
Santiago317 <60004091+Santiago317@users.noreply.github.com>
2021-06-01 17:14:23 -0500

    Adjunto Archivos .py de Funciones

---
+++ Ejercicios/LlamadoDeFunciones.py
@@ -0,0 +1,9 @@
+#Llamamos las funciones declaradas en otro archivo
+from FuncionesExternas import *
+
+#Invocamos las funciones
+funcion1()
+#Invocamos la otra funcion
+Q = input ('Introduzca un dato: ')
+W = input ('Introduzca otro dato: ')
+print(funcion2 (Q, W) )
